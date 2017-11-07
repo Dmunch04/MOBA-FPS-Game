f1f483d0e8204268e5c18a89a217cf77fd29a786
DaGreatCake <benjaminveld02@gmail.com>
2017-11-07 19:26:00 +0100

    Added control script
    I added a few control scripts, not done yet.

---
+++ FPS-MOBA Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public int visible = 0;
+	public float sensitivity = 100.0f;
+	public float clampAngle = 80.0f;
+	private float rotationX = 0.0f;
+	private float rotationY = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		Vector3 rot = transform.localRotation.eulerAngles;
+		rotationX = rot.x;
+		rotationY = rot.y;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float mouseX = Input.GetAxis ("Mouse X");
+		float mouseY = -Input.GetAxis ("Mouse Y");
+
+		rotationY += mouseX * sensitivity * Time.deltaTime;
+		rotationX += mouseY * sensitivity * Time.deltaTime;
+		rotationX = Mathf.Clamp(rotationX, -clampAngle, clampAngle);
+		Quaternion localRotation = Quaternion.Euler(rotationX, rotationY, 0.0f);
+		transform.rotation = localRotation;
+	}
+}
---
+++ FPS-MOBA Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject camera;
+	public float speed = 4.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 rotation = new Vector3 (gameObject.transform.eulerAngles.x, camera.transform.eulerAngles.y, gameObject.transform.eulerAngles.z);
+		gameObject.transform.eulerAngles = rotation;
+
+		if (Input.GetKey ("w")) {
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		}
+		if (Input.GetKey ("s")) {
+			transform.Translate (-Vector3.forward * speed * Time.deltaTime);
+		}
+		if (Input.GetKey ("a")) {
+			transform.Translate (Vector3.left * speed * Time.deltaTime);
+		}
+		if (Input.GetKey ("d")) {
+			transform.Translate (Vector3.right * speed * Time.deltaTime);
+		}
+	}
+}
